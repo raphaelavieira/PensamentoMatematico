d4902ab3514709411d817fd08450d55633ebf843
Raphaela Vieira <80799294+raphaelavieira@users.noreply.github.com>
2021-09-13 22:16:32 -0300

    Update ExercicioCinco.java

--- ExercicioCinco.java
+++ ExercicioCinco.java
@@ -1,29 +1,29 @@
-package atividadepratica;
-
 import java.util.Scanner;
 
-public class ExercicioCinco {
+public class Exercicio5 {
+    
     public static void main(String[] args) {
-        float precoRefrigerante = 3;
-        int qt_refrigerante = 0;
-        float totalrefrigerante = 0;
-        float precoGasolina = 2.50f;
-        float litros = 0;
-        float totalgasolina = 0;
-        float totalcompra = 0;
+	   
+        float precoGasolina = 2.5f;
+		float precoRefri = 2.0f;
+		float litrosGasolina = 0.0f;
+		int quantRefri = 0;
+		float totalGasolina = 0.0f;
+		float totalRefrigerante = 0.0f;
+        float totalCompra = 0;
 
-        Scanner teclado = new Scanner(System.in);
-
-        System.out.println("Quantidade de refrigerantes: ");
-        qt_refrigerante = teclado.nextInt();
-
-        System.out.println("Quantidade de litros de gasolina: ");
-        litros = teclado.nextFloat();
-
-        totalrefrigerante = precoRefrigerante * qt_refrigerante;
-        totalgasolina = precoGasolina * litros;
-        totalcompra = totalgasolina + totalrefrigerante;
-
-        System.out.println("Total a pagar = R$ " + totalcompra);
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Qual é a quantidade de litros de gasolina?: ");
+        litrosGasolina = ler.nextFloat();
+        System.out.println("Qual é a quantidade de refrigerantes?: ");
+        quantRefri = ler.nextInt();
+        
+        totalGasolina = precoGasolina * litrosGasolina;
+        totalRefrigerante = precoRefri * quantRefri;
+        
+        totalCompra = totalGasolina + totalRefrigerante;
+        
+        System.out.println("Total a pagar = R$ " + totalCompra);
     }
 }
