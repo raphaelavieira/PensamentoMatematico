2edbfb3e94a77672dcbbf21014b05919fbfdf07e
Raphaela Vieira <80799294+raphaelavieira@users.noreply.github.com>
2021-09-13 22:06:00 -0300

    Update ExercicioQuatro.java

--- ExercicioQuatro.java
+++ ExercicioQuatro.java
@@ -3,28 +3,31 @@ package atividadepratica;
 import java.util.Scanner;
 
 public class ExercicioQuatro {
+    
     public static void main(String[] args) {
+    
         float salario = 0;
         float aumento = 0.2f;
-        String escolha = "";
+        float novoSalario = 0;
+        String nome = "";
 
-        Scanner teclado = new Scanner(System.in);
+        Scanner ler = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Informe o valor do salário: ");
-            salario = Float.parseFloat(teclado.nextLine());
+            System.out.print("Digite o salário: R$ ");
+            salario = Float.parseFloat(ler.nextLine());
 
             if (salario <= 500){
-                salario += (salario * aumento);
-                System.out.println("Contemplado com aumento. Salário atual = R$: " + salario);
+                novoSalario = salario + (salario * aumento);
+                System.out.print("O novo salário é: R$ " + novoSalario);
             }
             else {
-                System.out.println("Salário não comtemplado com aumento (superior a R$ 500,00).");
+                System.out.println("Valor não contemplado para aumento, pois é superior a R$500,00");
             }
 
-            System.out.println("Tecle enter para continuar ou 'Fim' para sair.");
-            escolha = teclado.nextLine();
-            if (escolha.equalsIgnoreCase("Fim")){
+            System.out.println("Tecle enter para continuar ou 'Não' para sair.");
+            teclado = ler.nextLine();
+            if (teclado.equalsIgnoreCase("Não")){
                 System.exit(0);
             }
         }
